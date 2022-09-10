5ed8edbab6cddb8060f31d478292269897b32622
Bmalik73 <bmalik01@live.fr>
2022-09-11 00:06:57 +0200

    adding pages to my website

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/pages');
     eleventyConfig.addPassthroughCopy('./src/picture');
     eleventyConfig.addPassthroughCopy("./src/script.js");
 
